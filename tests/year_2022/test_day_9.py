1320f57448e668fd79013620df245e4592d312b2
Daniel <None>
2022-12-10 11:20:42 -0700

    Use mock fixture parameters for year 2022 day 9 test

--- tests/year_2022/test_day_9.py
+++ tests/year_2022/test_day_9.py
@@ -2,17 +2,21 @@
 from os import getcwd, path
 from unittest.mock import Mock
 
-from pytest import fixture
+from pytest import fixture, mark
 from pytest_mock import MockFixture
 
 from advent_of_code.year_2022 import day_9
 
 
 @fixture
-def mock_get_input(mocker: MockFixture) -> Mock:
+def mock_get_input(mocker: MockFixture, part: int) -> Mock:
     """It mocks the input function and returns the test input instead."""
     input_file = path.join(
-        getcwd(), "tests", "year_2022", "test_inputs", "day_9_input_1.txt"
+        getcwd(),
+        "tests",
+        "year_2022",
+        "test_inputs",
+        "day_9_input_{part}.txt".format(part=part),
     )
     mock = mocker.patch("advent_of_code.advent_of_code_requests.get_input")
     with open(input_file, "r") as f:
@@ -20,33 +24,25 @@ def mock_get_input(mocker: MockFixture) -> Mock:
     return mock
 
 
-@fixture
-def mock_get_input_2(mocker: MockFixture) -> Mock:
-    """It mocks the input function and returns the test input instead."""
-    input_file = path.join(
-        getcwd(), "tests", "year_2022", "test_inputs", "day_9_input_2.txt"
-    )
-    mock = mocker.patch("advent_of_code.advent_of_code_requests.get_input")
-    with open(input_file, "r") as f:
-        mock.return_value = f.read().rstrip("\n")
-    return mock
-
-
+@mark.parametrize("part", [1])
 def test_solution_1(mock_get_input: Mock) -> None:
     """It verifies the first part with a test input."""
     assert day_9.count_distinct_coordinates_for_last_knot(knot_count=2) == 13
 
 
+@mark.parametrize("part", [1])
 def test_solution_2_part_1(mock_get_input: Mock) -> None:
     """It verifies the second part with a test input."""
     assert day_9.count_distinct_coordinates_for_last_knot(knot_count=10) == 1
 
 
-def test_solution_2_part_2(mock_get_input_2: Mock) -> None:
+@mark.parametrize("part", [2])
+def test_solution_2_part_2(mock_get_input: Mock) -> None:
     """It verifies the second part with a test input."""
     assert day_9.count_distinct_coordinates_for_last_knot(knot_count=10) == 36
 
 
+@mark.parametrize("part", [1])
 def test_main(mock_get_input: Mock) -> None:
     """It verifies both parts with the test input in the test input document."""
     assert day_9.main() == (13, 1)
